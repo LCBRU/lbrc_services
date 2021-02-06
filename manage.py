0a930ce7a00ff7ccebc7408c5e10cb5a4b6079bc
Richard Bramley <rabramley@gmail.com>
2021-02-06 17:52:21 +0000

    Changes to get it working on UoL LAMPS

--- manage.py
+++ manage.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+from dotenv import load_dotenv
+
+# UoL LAMP servers (python 3.6?) seem to need me to load dotenv here
+
+load_dotenv()
+
 from migrate.versioning.shell import main
 from lbrc_flask.config import BaseConfig
 
