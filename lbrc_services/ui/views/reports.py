35d0c5c06294d6e04458d38ff942d67ffc6ed51f
Richard Bramley <rabramley@gmail.com>
2021-05-06 10:57:28 +0100

    tasks by status

--- lbrc_services/ui/views/reports.py
+++ lbrc_services/ui/views/reports.py
@@ -6,9 +6,11 @@ from dateutil.rrule import rrule, MONTHLY
 from datetime import date
 from flask import render_template
 from flask_security import current_user
-from lbrc_services.model import Task, Service
+from lbrc_services.model import Task, Service, TaskStatusType
+from sqlalchemy.sql import func
 from ..decorators import must_own_a_service
 from .. import blueprint
+from lbrc_flask.database import db
 
 @blueprint.route("/reports")
 @must_own_a_service()
@@ -16,15 +18,22 @@ def reports():
     return render_template("ui/reports.html")
 
 
-@blueprint.route("/reports/by_date")
+def sqlalchemy_format_date():
+    print(db.session.bind.dialect.name)
+    return None
+
+@blueprint.route("/reports/service_tasks_requested_by_month")
 @must_own_a_service()
-def report_by_date():
+def service_tasks_requested_by_month():
+
+    date_formatter = sqlalchemy_format_date()
 
     tasks = Task.query.with_entities(
         Service.name,
         Task.created_date,
+        func.strftime('%b %Y',Task.created_date),
     ).join(
-        Task.service
+        Task.service,
     ).filter(
         Task.service_id.in_([u.id for u in current_user.owned_services])
     ).order_by(
@@ -35,22 +44,52 @@ def report_by_date():
     min_date = min([t[1] for t in tasks])
     max_date = max([t[1] for t in tasks])
 
-    buckets = list(rrule(
+    buckets = [d.strftime('%b %Y') for d in rrule(
         MONTHLY,
         dtstart=date(min_date.year, min_date.month, 1),
         until=date(max_date.year, max_date.month, 1),
-    ))
+    )]
 
     chart = pygal.Bar(style=Style(font_family='Lato'))
-    chart.title = 'Service Tasks by Requested Month'
-    chart.x_labels = [b.strftime('%b %Y') for b in buckets]
+    chart.title = 'Service Tasks Requested by Month'
+    chart.x_labels = buckets
 
     for service_name, service_tasks in groupby(tasks, lambda t: t[0]):
-        count = Counter([(t[1].year, t[1].month) for t in service_tasks])
-        month_range_count = {
-            d.strftime('%b %Y'): count.get((d.year, d.month), 0)
-            for d in buckets}
+        count = Counter([t[1].strftime('%b %Y') for t in service_tasks])
+        month_range_count = {b: count.get(b, 0) for b in buckets}
 
         chart.add(service_name, month_range_count.values())
+
+    return chart.render_response()
+
+
+@blueprint.route("/reports/service_tasks_by_current_status")
+@must_own_a_service()
+def service_tasks_by_current_status():
+
+    tasks = Task.query.with_entities(
+        Service.name,
+        TaskStatusType.name,
+    ).join(
+        Task.service,
+        Task.current_status_type,
+    ).filter(
+        Task.service_id.in_([u.id for u in current_user.owned_services])
+    ).order_by(
+        Service.name,
+        Task.created_date,
+    ).all()
+
+    buckets = {t[1] for t in tasks}
+
+    chart = pygal.Bar(style=Style(font_family='Lato'))
+    chart.title = 'Service Tasks by Current Status'
+    chart.x_labels = buckets
+
+    for service_name, service_tasks in groupby(tasks, lambda t: t[0]):
+        count = Counter([t[1] for t in service_tasks])
+        values = {b: count.get(b, 0) for b in buckets}
+
+        chart.add(service_name, values.values())
 
     return chart.render_response()
