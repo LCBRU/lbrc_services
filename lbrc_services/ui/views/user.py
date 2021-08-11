2f52f1cf730f752d6a7b5dd37895feb3941255ca
Richard Bramley <rabramley@gmail.com>
2021-08-11 14:43:34 +0100

    Search local and LDAP users

--- lbrc_services/ui/views/user.py
+++ lbrc_services/ui/views/user.py
@@ -1,6 +1,8 @@
+from sqlalchemy import func, or_
 from lbrc_flask.security.ldap import Ldap
-from lbrc_services.model import Task
+from lbrc_services.model import Task, User
 from lbrc_flask.requests import get_value_from_all_arguments
+from lbrc_flask.database import db
 from .. import blueprint
 
 
@@ -14,18 +16,73 @@ def user_search():
         l = Ldap()
         l.login_nonpriv()
 
-        users = sorted(l.search_user(q), key=lambda u: (u['surname'], u['given_name']))
+        users = {}
 
-        for u in users:
-            results.append({
-                'id': u['username'],
-                'text': '{} {} ({})'.format(
-                    u['given_name'],
-                    u['surname'],
-                    u['username'],
-                ),
+        if db.session.bind.dialect.name == 'sqlite':
+            query = User.query.filter(
+                or_(
+                    User.username.like(f'%{q}%'),
+                    or_(
+                        User.first_name.like(f'%{q}%'),
+                        or_(
+                            User.last_name.like(f'%{q}%'),
+                            User.email.like(f'%{q}%'),
+                        )
+                    )
+                )
+            )
+        elif db.session.bind.dialect.name == 'mysql':
+            query = User.query.filter(
+                or_(
+                    User.username.like(f'%{q}%'),
+                    or_(
+                        func.concat(User.first_name, ' ', User.last_name).like(f'%{q}%'),
+                        User.email.like(f'%{q}%'),
+                    )
+                )
+            )
 
-            })
+        for u in query.all():
+            users[u.username] = {
+                'id': u.id,
+                'username': u.username,
+                'full_name': u.full_name,
+                'first_name': u.first_name,
+                'last_name': u.last_name,
+            }
+
+        for u in l.search_user(q):
+            if u['username'] not in users:
+                users[u['username']] = {
+                    'id': u['username'],
+                    'username': u['username'],
+                    'full_name': '{} {} ({})'.format(
+                        u['given_name'],
+                        u['surname'],
+                        u['username'],
+                    ),
+                    'first_name': u['given_name'],
+                    'last_name': u['surname'],
+                }
+
+        users = sorted(users.values(), key=lambda u: (u['last_name'], u['first_name']))
+
+        results = [{
+            'id': u['id'],
+            'text': u['full_name'],
+        } for u in users]
+
+        # for u in User.query.filter(or_(
+        #     User.username.like(f'%{q}%'),
+        #     or_(
+        #         func.concat(User.first_name, ' ', User.last_name).like(f'%{q}%'),
+        #         User.email.like(f'%{q}%'),
+        #     )
+        # )).all():
+        #     results.append({
+        #         'id': u.id,
+        #         'text': u.full_name,
+        #     })
 
     return {'results': results}
 
