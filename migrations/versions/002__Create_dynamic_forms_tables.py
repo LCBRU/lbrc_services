868396a2341735bae4cd2a8c7a22abc045527a55
Richard Bramley <rabramley@gmail.com>
2023-03-21 12:14:37 +0000

    Remove SQLAlchemy Migrate

--- migrations/versions/002__Create_dynamic_forms_tables.py
+++
@@ -1,8 +0,0 @@
-from lbrc_flask.forms.migrations import create_dynamic_form_tables, drop_security_tables
-
-
-def upgrade(migrate_engine):
-    create_dynamic_form_tables(migrate_engine)
-
-def downgrade(migrate_engine):
-    drop_security_tables(migrate_engine)
